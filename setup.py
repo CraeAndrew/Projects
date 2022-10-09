f56df5c39e5be4c3d7faebd30eec170d89a339c3
CraeAndrew <115107926+CraeAndrew@users.noreply.github.com>
2022-10-09 12:55:51 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'Project1'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*_launch.py'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='crae',
+    maintainer_email='craeandrew@ufl.edu',
+    description='Controller Mapping',
+    license='N/A',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        
+        ],
+    },
+)
