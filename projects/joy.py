a1392d2ab8b1f13af3ec15ca75d334e332c92e4f
CraeAndrew <115107926+CraeAndrew@users.noreply.github.com>
2022-10-09 14:35:12 -0400

    Update joy.py

--- projects/joy.py
+++ projects/joy.py
@@ -1,33 +1,31 @@
 import rclpy
 from rclpy.node import Node
 
-from sensor_msgs.msg import LaserScan
-from sensor_msgs.msg import PointCloud
-
+from sensor_msgs.msg import Joy
 from std_msgs.msg import Int16
-
-from geometry_msgs.msg import Point32
-
+#from drive_interfaces.msg import VehCmd
+from geometry_msgs.msg import Twist
+#from .ports import ports
 import array
 import math
 
 
-class Joy(Node):
+class Joy_Count(Node):
 
     def __init__(self):
         super().__init__('joy')
         self.subscription = self.create_subscription(
-            Int16,'Joy',self.listener_callback,10)
+            Joy,'joy',self.listener_callback,10)
         self.subscription  # prevent unused variable warning
 
     def listener_callback(self, msg):
-        self.get_logger().info('"%s"' % msg.data)
+        self.get_logger().info('"%s"' % msg)
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    joy = Joy()
+    joy = Joy_Count()
 
     rclpy.spin(joy)
 
