6dc465d9188f9fc18b6f9cf71ea27761174f5858
Stefan Kieszkowski <stefan.stefan4444@gmail.com>
2022-10-14 14:32:05 -0400

    "launch stuff"

--- Launch.py
+++ Launch.py
@@ -5,10 +5,26 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     ld = LaunchDescription()
+
     joy_node = Node(
+        package="joy",
+        executable="joy_node",
+        #parameters=[{"steering_offset": 0.0}],
+    )
+    ld.add_action(joy_node)
+
+    joy = Node(
         package="projects",
         executable="joy",
         #parameters=[{"steering_offset": 0.0}],
     )
-    ld.add_action(joy_node)
+    ld.add_action(joy)
+
+    motor_controller_node = Node(
+        package="motor_driver",
+        executable="motor_controller",
+        parameters=[{"steering_offset": 0.0}],
+    )
+    ld.add_action(motor_controller_node)
+
     return ld
